44f9ab96749332c7b15c53a1210c2d627075a81d
qqhsx <378600950@qq.com>
2024-07-10 14:42:56 +0800

    Site updated: 2024-07-10 14:42:46

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -4,30 +4,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const bitcoinPriceDiv = document.getElementById('bitcoin-price');
     const dailyNewsDiv = document.getElementById('daily-news');
 
-    const bitcoinPriceRequest = fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => response.json());
+    function fetchData() {
+        const bitcoinPriceRequest = fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => response.json());
 
-    const holidayInfoRequest = fetch('https://timor.tech/api/holiday/tts')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('网络响应失败');
-            }
-            return response.json();
-        });
+        const holidayInfoRequest = fetch('https://timor.tech/api/holiday/tts')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('网络响应失败');
+                }
+                return response.json();
+            });
 
-    Promise.all([bitcoinPriceRequest, holidayInfoRequest])
-        .then(([bitcoinData, holidayData]) => {
-            // 处理比特币价格数据
-            const usdRate = bitcoinData.bpi.USD.rate;
-            bitcoinPriceDiv.innerHTML = `当前比特币价格：$${usdRate}`;
+        Promise.all([bitcoinPriceRequest, holidayInfoRequest])
+            .then(([bitcoinData, holidayData]) => {
+                // 处理比特币价格数据
+                const usdRate = bitcoinData.bpi.USD.rate;
+                bitcoinPriceDiv.innerHTML = `当前比特币价格：$${usdRate}`;
 
-            // 处理节假日信息数据
-            const holidayMessage = holidayData.tts;
-            dailyNewsDiv.innerHTML = `<p>${holidayMessage}</p>`;
-        })
-        .catch(error => {
-            console.error('获取数据失败:', error);
-            bitcoinPriceDiv.innerHTML = '获取比特币价格失败';
-            dailyNewsDiv.innerHTML = '获取节假日信息失败';
-        });
+                // 处理节假日信息数据
+                const holidayMessage = holidayData.tts;
+                dailyNewsDiv.innerHTML = `<p>${holidayMessage}</p>`;
+            })
+            .catch(error => {
+                console.error('获取数据失败:', error);
+                bitcoinPriceDiv.innerHTML = '获取比特币价格失败';
+                dailyNewsDiv.innerHTML = '获取节假日信息失败';
+            });
+    }
+
+    // 初次加载数据
+    fetchData();
+
+    // 设置自动刷新，每分钟刷新一次
+    //setInterval(fetchData, 60000);
+	setInterval(fetchData, 100);
 });
